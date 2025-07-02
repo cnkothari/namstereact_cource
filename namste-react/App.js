7952c67697fb7258e77bcb176679736f1f54723d
cnkothari <cnt.231107@gmail.com>
2025-07-02 09:49:30 +0530

    notes and code of episode -3 (laying the foundation)

--- namste-react/App.js
+++ namste-react/App.js
@@ -1,74 +1,31 @@
-//  createElement needs 3 argument  firstone is tag a second one Object and third one what we need to pass in tag , Here in object we can give attributes like class,id,etc 
+import React from "react";
+import  ReactDOM from "react-dom/client";
 
-// React.createElement is just object
+const elm = <span>Element </span>
 
-// Nested Html inside React
-
-{/* <div id="paremt">
-  <div id="child">
-    <h1 >I am h1 tag !</h1>
-    <h2 >I am h1 tag !</h2>
-  </div>
-  <div id="child2">
-    <h1 >I am h1 tag !</h1>
-    <h2 >I am h1 tag !</h2>
-  </div>
-</div>
-ReactElement(object) => Html (Browser understad) */
-//  to pass sibling we need to pass array
-}
-
-const parent = React.createElement(
-    'div',
-    {id:'paremt'},
-    [
-        React.createElement(
-            'div',
-            {id:'child'},
-            [
-                React.createElement(
-                    'h1',
-                    {},
-                    "I am h1 tag !"
-                ),
-                React.createElement(
-                    'h2',
-                    {},
-                    "I am h2 tag !"
-                )
-            ]
-             
-        ),
-        React.createElement(
-            'div',
-            {id:'child2'},
-            [
-                React.createElement(
-                    'h1',
-                    {},
-                    "I am h1 tag !"
-                ),
-                React.createElement(
-                    'h2',
-                    {},
-                    "I am h2 tag !"
-                )
-            ]
-             
-        )
-    ]
-
-);
-console.log(parent);
-const heading = React.createElement(
-    "h1" ,
-    {id: 'heading',xyz: 'abc'} ,
-    "Hello World From React!" 
+const title =  (
+    <h1 className="head" tabIndex="5">
+          React Using JSX !
+    </h1>
 );
 
-//console.log(heading); //returns Object
-const root =  ReactDOM.createRoot(document.getElementById("root"));
+const number = 1000;
 
-// root.render's job is take object and put it up and convert object in h1 tag and put in dom
-//root.render replace the content inside root div
-root.render(parent);
+const HeadingComponent = () => (
+    <div id ="container">
+        <h2>{number}</h2>
+        <h2>{100 +200}</h2>
+        
+        {title}
+        <h1 className="heading" >
+             Namste React Functional component!
+        </h1>
+
+    </div>
+);
+
+const root =  ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(<HeadingComponent />);
---
+++ namste-react/README.md
@@ -0,0 +1,22 @@
+# namstereact_cource
+
+# Parcel
+- create Dev Build
+- Create local Server 
+- HMR (Auto refresh page ) = Hot Module Replacement
+- using file watching algorithm -> written in C++
+- Faster builds as Parcel caching things (.parcel-cache files are binary file and so dev build are faster)
+- Parcel is a zero-config bundler
+- Parcel also do Image optimization
+- Parcel also do minification of our files
+- Parcel also do bundling our files
+- Parcel also do Compress our files
+- use Consistent hashing
+- Parcel also do code spliting
+- Parcel also do differential bundling ( support older browser )
+- Parcel also do diagnostic of app
+- Parcel also gives good error handling 
+- Parcel also gives a way to host app on https
+- supports Tree Shaking algorithm  (removes unuse code)
+- different build for dev and prod bundle
+
